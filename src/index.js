ba09886ebc2f06a78f0484d96a9efd4cc01349e7
NicholasNeto <nicholasneto@outlook.com>
2021-11-10 21:34:30 -0300

    created middleware findUserById

--- src/index.js
+++ src/index.js
@@ -40,7 +40,15 @@ function checksTodoExists(request, response, next) {
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+  const user = users.find((user) => user.id === id)
+
+  if (!user) {
+    return response.status(400).json({ error: 'User not found' })
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
